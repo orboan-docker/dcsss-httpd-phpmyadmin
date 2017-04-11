cb50624e9b76b727737ff8a2c00cf8866940e060
vagrant <vagrant@ubuntu.local>
2017-04-11 09:04:34 +0200

    Modified domain certs

--- container-files/config/init/z_import_mysql_tables.sh
+++ container-files/config/init/z_import_mysql_tables.sh
@@ -4,4 +4,6 @@ pwd=$MYSQL_ROOT_PASSWORD
 
 host=$MYSQL_HOST
 
-for i in {1..5}; do "mysql -h${host} -uroot -p${pwd} < /var/www/html/sql/create_tables.sql" && break || sleep 10; done
+for i in {1..5}; do
+mysql -h${host} -uroot -p${pwd} < /var/www/html/sql/create_tables.sql && break || sleep 10;
+done
